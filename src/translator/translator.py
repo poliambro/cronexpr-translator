c9b44ad51701fa183e4f721a6c60f86335433d7e
PolianaNeves <poliana.ambrosio.campos@gmail.com>
2022-06-15 22:27:14 -0300

    add minutes translation and refactorings

--- src/translator/translator.py
+++ src/translator/translator.py
@@ -1,62 +1,68 @@
+from src.translator.enums import CronField, AllowedCharacters
+
+
 class Translator:
 
     @staticmethod
-    def translate_seconds(seconds_subexpression: str) -> str:
-        if Translator.__is_star_expression(seconds_subexpression):
-            return "every second"
-        if Translator.__has_slash_in_expression(seconds_subexpression):
-            return Translator.__get_slash_translation(seconds_subexpression)
-        if Translator.__is_list_expression(seconds_subexpression):
-            return Translator.__get_list_translation(seconds_subexpression)
-        return Translator.__get_range_translation(seconds_subexpression)
+    def translate_seconds_and_minutes(subexpression: str, field_type: CronField) -> str:
+        field_name = str(field_type.name).lower()
+        past_time = f" past the {str(CronField(field_type.value + 1).name).lower()}"
+        if Translator.__is_star_expression(subexpression):
+            return f"every {field_name}"
+        if Translator.__has_slash_in_expression(subexpression):
+            return Translator.__get_slash_translation(subexpression, field_name, past_time)
+        if Translator.__is_list_expression(subexpression):
+            return Translator.__get_list_translation(subexpression, field_name, past_time)
+        return Translator.__get_range_translation(subexpression, field_name, past_time)
 
     @staticmethod
     def __is_star_expression(subexpression: str) -> bool:
-        return "*" == subexpression
+        return AllowedCharacters.STAR.value == subexpression
 
     @staticmethod
     def __is_slashed_star_expression(subexpression: str) -> bool:
-        return "*/" in subexpression or "0/" in subexpression
+        return f"{AllowedCharacters.STAR.value}{AllowedCharacters.SLASH.value}" in subexpression or \
+               f"0{AllowedCharacters.SLASH.value}" in subexpression
 
     @staticmethod
     def __has_slash_in_expression(subexpression: str) -> bool:
-        return "/" in subexpression
+        return AllowedCharacters.SLASH.value in subexpression
 
     @staticmethod
     def __is_list_expression(subexpression: str) -> bool:
-        return "," in subexpression
+        return AllowedCharacters.LIST.value in subexpression
 
     @staticmethod
     def __is_range_expression(subexpression: str) -> bool:
-        return "-" in subexpression
+        return AllowedCharacters.RANGE.value in subexpression
 
     @staticmethod
     def __get_element_description(element: str) -> str:
         if Translator.__is_range_expression(element):
-            arguments = element.rsplit("-")
+            arguments = element.rsplit(AllowedCharacters.RANGE.value)
             return f"{arguments[0]} through {arguments[1]}"
         return element
 
     @staticmethod
-    def __get_list_translation(subexpression: str) -> str:
-        list_values = subexpression.rsplit(",")
+    def __get_list_translation(subexpression: str, field_name: str, past_time: str = "") -> str:
+        list_values = subexpression.rsplit(AllowedCharacters.LIST.value)
         translation_msg = "at "
         for value in list_values:
             element_desc = Translator.__get_element_description(value)
             if list_values[-1] == value:
-                translation_msg += f"and {element_desc} seconds past the minute"
+                translation_msg += f"and {element_desc} {field_name}s{past_time}"
                 break
             translation_msg += f"{element_desc}, "
         return translation_msg
 
     @staticmethod
-    def __get_slash_translation(subexpression: str) -> str:
-        arguments = subexpression.rsplit("/")
+    def __get_slash_translation(subexpression: str, field_name: str, past_time: str = "") -> str:
+        arguments = subexpression.rsplit(AllowedCharacters.SLASH.value)
+        slash_expr = f"every {arguments[1]} {field_name}s"
         if Translator.__is_slashed_star_expression(subexpression):
-            return f"every {arguments[1]} seconds"
-        return f"every {arguments[1]} seconds, starting at second {arguments[0]} past the minute"
+            return slash_expr
+        return f"{slash_expr}, starting at {field_name} {arguments[0]}{past_time}"
 
     @staticmethod
-    def __get_range_translation(subexpression: str) -> str:
-        return f"seconds {Translator.__get_element_description(subexpression)} past the minute"
-
+    def __get_range_translation(subexpression: str, field_name: str, past_time: str = "") -> str:
+        return f"{field_name}s {Translator.__get_element_description(subexpression)}{past_time}"
--- tests/translator/test_translator.py
+++ tests/translator/test_translator.py
@@ -1,45 +1,62 @@
 import unittest
 from src.translator.translator import Translator
+from src.translator.enums import CronField
 
 
 class TestTranslator(unittest.TestCase):
 
-    def test_should_translate_seconds_star_subexpression(self):
-        seconds_subexpression = "*"
-        translated_expression = Translator.translate_seconds(seconds_subexpression)
+    def test_should_translate_seconds_and_minutes_star_subexpression(self):
+        sec_and_min_subexpression = "*"
+        translated_expression = Translator.translate_seconds_and_minutes(sec_and_min_subexpression, CronField.SECOND)
         self.assertEquals(translated_expression, "every second")
+        translated_expression = Translator.translate_seconds_and_minutes(sec_and_min_subexpression, CronField.MINUTE)
+        self.assertEquals(translated_expression, "every minute")
 
-    def test_should_translate_seconds_star_with_slash_subexpression(self):
+    def test_should_translate_seconds_and_minutes_star_with_slash_subexpression(self):
         seconds_subexpression = "*/5"
-        translated_expression = Translator.translate_seconds(seconds_subexpression)
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.SECOND)
         self.assertEquals(translated_expression, "every 5 seconds")
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.MINUTE)
+        self.assertEquals(translated_expression, "every 5 minutes")
 
-    def test_should_translate_seconds_slash_subexpression(self):
+    def test_should_translate_seconds_and_minutes_slash_subexpression(self):
         seconds_subexpression = "5/20"
-        translated_expression = Translator.translate_seconds(seconds_subexpression)
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.SECOND)
         self.assertEquals(translated_expression, "every 20 seconds, starting at second 5 past the minute")
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.MINUTE)
+        self.assertEquals(translated_expression, "every 20 minutes, starting at minute 5 past the hour")
 
-    def test_should_translate_seconds_slash_subexpression_to_every_second_when_the_first_value_is_0(self):
+    def test_should_translate_seconds_and_minutes_slash_subexpression_to_every_second_when_the_first_value_is_0(self):
         seconds_subexpression = "0/20"
-        translated_expression = Translator.translate_seconds(seconds_subexpression)
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.SECOND)
         self.assertEquals(translated_expression, "every 20 seconds")
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.MINUTE)
+        self.assertEquals(translated_expression, "every 20 minutes")
 
-    def test_should_translate_seconds_range_subexpression(self):
+    def test_should_translate_seconds_and_minutes_range_subexpression(self):
         seconds_subexpression = "5-10"
-        translated_expression = Translator.translate_seconds(seconds_subexpression)
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.SECOND)
         self.assertEquals(translated_expression, "seconds 5 through 10 past the minute")
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.MINUTE)
+        self.assertEquals(translated_expression, "minutes 5 through 10 past the hour")
 
-    def test_should_translate_seconds_list_subexpression(self):
+    def test_should_translate_seconds_and_minutes_list_subexpression(self):
         seconds_subexpression = "0,5,10,15,20"
-        translated_expression = Translator.translate_seconds(seconds_subexpression)
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.SECOND)
         self.assertEquals(translated_expression, "at 0, 5, 10, 15, and 20 seconds past the minute")
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.MINUTE)
+        self.assertEquals(translated_expression, "at 0, 5, 10, 15, and 20 minutes past the hour")
 
-    def test_should_translate_seconds_list_with_range_within_subexpression(self):
+    def test_should_translate_seconds_and_minutes_list_with_range_within_subexpression(self):
         seconds_subexpression = "0,5,10-15,20"
-        translated_expression = Translator.translate_seconds(seconds_subexpression)
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.SECOND)
         self.assertEquals(translated_expression, "at 0, 5, 10 through 15, and 20 seconds past the minute")
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.MINUTE)
+        self.assertEquals(translated_expression, "at 0, 5, 10 through 15, and 20 minutes past the hour")
 
-    def test_should_translate_seconds_list_with_range_within_subexpression_when_a_range_value_is_the_last(self):
+    def test_should_translate_seconds_and_minutes_list_with_range_within_subexpression_when_a_range_value_is_the_last(self):
         seconds_subexpression = "0,5,10-15,20-25"
-        translated_expression = Translator.translate_seconds(seconds_subexpression)
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.SECOND)
         self.assertEquals(translated_expression, "at 0, 5, 10 through 15, and 20 through 25 seconds past the minute")
+        translated_expression = Translator.translate_seconds_and_minutes(seconds_subexpression, CronField.MINUTE)
+        self.assertEquals(translated_expression, "at 0, 5, 10 through 15, and 20 through 25 minutes past the hour")
